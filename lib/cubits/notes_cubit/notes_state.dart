f358f6024198618e518d82728b9294a5f58e2d85
Raghda Magdy <raghdaelsaid32@gmail.com>
2023-01-31 11:27:49 +0200

    refresh notes list

--- lib/cubits/notes_cubit/notes_state.dart
+++ lib/cubits/notes_cubit/notes_state.dart
@@ -5,3 +5,4 @@ abstract class NotesState {}
 
 class NotesInitial extends NotesState {}
 
+class NotesSuccess extends NotesState {}
--- lib/views/widgets/add_note_bottom.dart
+++ lib/views/widgets/add_note_bottom.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:notes_app/constants.dart';
 import 'package:notes_app/cubits/add_cubit/add_note_cubitt.dart';
+import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/views/widgets/custom_textField.dart';
 
 import 'add_note_form.dart';
@@ -19,11 +20,11 @@ class AddNoteBottomSheet extends StatelessWidget {
       child: BlocConsumer<AddNoteCubitCubit, AddNoteCubitState>(
         listener: (context, state) {
           if (state is AddNoteCubitSuccess) {
+            BlocProvider.of<NotesCubit>(context).fetchAllNotes();
+
             Navigator.pop(context);
           }
-          if (state is AddNoteCubitFailure) {
-            print('failed  ${state.errMessage}');
-          }
+          if (state is AddNoteCubitFailure) {}
         },
         builder: (context, state) {
           return AbsorbPointer(
